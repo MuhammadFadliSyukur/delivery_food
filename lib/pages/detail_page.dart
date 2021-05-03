886160ae8c7584eb1cd59ced4077b05633aad8dc
Muhammad Fadli Syukur <muhammadfadlisyukur16@gmail.com>
2021-05-04 03:09:28 +0700

    first commit

---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,15 @@
+//import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text('Detail Area'),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/popular_food_card.dart
+++ lib/widgets/popular_food_card.dart
@@ -1,4 +1,5 @@
 import 'package:delivery_food/models/popular_food_model.dart';
+import 'package:delivery_food/pages/detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../theme.dart';
@@ -10,44 +11,54 @@ class PopularFoodCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 315.w,
-      height: 120.h,
-      decoration: BoxDecoration(
-        color: greyColor,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: edge.w),
-        child: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child: Image.asset(
-                popularFood.imgUrl,
-                width: 70.w,
-                height: 70.h,
-                fit: BoxFit.cover,
-              ),
-            ),
-            SizedBox(width: 18.w),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(popularFood.name, style: blackTextStyle),
-                Text(
-                  popularFood.category,
-                  style: TextStyle(
-                      color: darkGreyColor,
-                      fontSize: 12.sp,
-                      fontWeight: FontWeight.w500),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailPage(),
+          ),
+        );
+      },
+      child: Container(
+        width: 315.w,
+        height: 120.h,
+        decoration: BoxDecoration(
+          color: greyColor,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: edge.w),
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.asset(
+                  popularFood.imgUrl,
+                  width: 70.w,
+                  height: 70.h,
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(height: 7.h),
-                Text('IDR ${popularFood.price}', style: blackTextStyle),
-              ],
-            )
-          ],
+              ),
+              SizedBox(width: 18.w),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(popularFood.name, style: blackTextStyle),
+                  Text(
+                    popularFood.category,
+                    style: TextStyle(
+                        color: darkGreyColor,
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(height: 7.h),
+                  Text('IDR ${popularFood.price}', style: blackTextStyle),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
